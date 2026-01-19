483566aeef765c5822a6241cd99ceaebefc22b05
Andrew Latimer <amlatimer@ucdavis.edu>
2026-01-18 16:22:46 -0800

    add demo script with intro info

---
+++ scripts/brms-intitial-demo-script.R
@@ -0,0 +1,15 @@
+#### Script to work through how to use brms to fit a straightforward linear model ####
+# 
+# Workshop for lab meeting January 20, 2026
+# 
+# 1) compare linear mixed model using lme4 vs brms 
+# 2) discuss the main brms settings and reason for them 
+# 3) checking models for convergence 
+# 4) some other model checks
+# 5) a few model results plots 
+# 6) posterior predictive checks
+# 7) model comparison and evaluation
+
+
+
+
